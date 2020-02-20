37e9f42baa3d7017b26f3fe758a629d55724c27b
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2020-02-20 07:56:39 +0100

    init project and implement component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import "./Animbg.css";
 import Animbg from "./Animbg";
 
 function App() {
@@ -11,6 +12,48 @@ function App() {
           className="anim-bg flying-cloud"
           ratioX="0.36"
           ratioY="0.34"
+        />
+        <Animbg
+          src="./assets/cloud.png"
+          className="anim-bg flying-cloud"
+          ratioX="0.25"
+          ratioY="0.23"
+          alt="cloud"
+        />
+        <Animbg
+          src="./assets/cloud.png"
+          className="anim-bg flying-cloud"
+          ratioX="0.19"
+          ratioY="0.38"
+          alt="cloud"
+        />
+        <Animbg
+          src="./assets/cloud.png"
+          className="anim-bg flying-cloud"
+          ratioX="0.11"
+          ratioY="0.09"
+          alt="cloud"
+        />
+        <Animbg
+          src="./assets/cloud.png"
+          className="anim-bg flying-cloud"
+          ratioX="0.13"
+          ratioY="0.53"
+          alt="cloud"
+        />
+        <Animbg
+          src="./assets/cloud.png"
+          className="anim-bg flying-cloud"
+          ratioX="0.23"
+          ratioY="0.42"
+          alt="cloud"
+        />
+        <Animbg
+          src="./assets/balloon.png"
+          className="anim-bg flying-balloon"
+          ratioX="0.07"
+          ratioY="0.31"
+          alt=""
         />
       </div>
     </div>
